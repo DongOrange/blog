133efbaeba5b1264483199dea98e7efbd14b419f
Og <breezenest0912@gmail.com>
2024-10-03 18:35:04 +0800

    style: change indentation

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+
 from . import views
 
-app_name="blog"
+app_name = "blog"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('new/', views.new, name='new'),
-    path('<int:id>/edit/', views.edit, name='edit'),
-    path('<int:id>/show/', views.show, name='show'),
+    path("", views.index, name="index"),
+    path("new/", views.new, name="new"),
+    path("<int:id>/edit/", views.edit, name="edit"),
+    path("<int:id>/show/", views.show, name="show"),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,33 +1,38 @@
-from django.shortcuts import render,redirect,get_object_or_404
-from .models import Post
+from django.shortcuts import get_object_or_404, redirect, render
+
 from .form import PostForm
+from .models import Post
+
 
 def index(request):
-    if request.method == "POST" :
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('blog:index')
-        return render(request,"blog/new.html",{'form':form})
+            return redirect("blog:index")
+        return render(request, "blog/new.html", {"form": form})
 
     posts = Post.objects.order_by("-id")
-    return render(request,"blog/index.html",{'posts':posts})
+    return render(request, "blog/index.html", {"posts": posts})
+
 
 def new(request):
     form = PostForm()
-    return render(request,"blog/new.html",{'form':form})
+    return render(request, "blog/new.html", {"form": form})
 
-def edit(request,id):
-    post = get_object_or_404(Post,id=id)
+
+def edit(request, id):
+    post = get_object_or_404(Post, id=id)
     form = PostForm(instance=post)
-    return render(request,'blog/edit.html',{'post':post,'form':form})
+    return render(request, "blog/edit.html", {"post": post, "form": form})
 
-def show(req,id):
-    post = get_object_or_404(Post,id=id)
+
+def show(req, id):
+    post = get_object_or_404(Post, id=id)
     if req.method == "POST":
-        form =PostForm(req.POST,instance=post)
+        form = PostForm(req.POST, instance=post)
         if form.is_valid():
             form.save()
-            return redirect('blog:show',id=id)
-        return render(req, 'blog/edit.html',{'form':form,'post':post})
-    return render(req, 'blog/show.html',{'post':post})
+            return redirect("blog:show", id=id)
+        return render(req, "blog/edit.html", {"form": form, "post": post})
+    return render(req, "blog/show.html", {"post": post})
